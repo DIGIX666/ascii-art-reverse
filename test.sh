e583395a7ef84003fc0be36b4e6ea8d572fca6a7
théo dub <theodub@MacThox.local>
2023-07-06 15:45:03 +0200

    all project

---
+++ test.sh
@@ -0,0 +1,27 @@
+go run . --reverse example00.txt
+
+go run . --reverse=example00.txt
+
+go run . --reverse=example01.txt
+
+go run . --reverse=example02.txt
+
+go run . --reverse=example03.txt
+
+go run . --reverse=example04.txt
+
+go run . --reverse=example05.txt
+
+go run . --reverse=example06.txt
+
+go run . --reverse=example07.txt
+
+go run . --reverse=example08.txt
+
+go run . --reverse=example09.txt
+
+go run . --reverse=example10.txt
+
+go run . --reverse=example11.txt
+
+echo "Tests finished!"
